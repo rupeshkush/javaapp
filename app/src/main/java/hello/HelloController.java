78e4f57c231e12a5c17aa2a2b77a25a53f95221a
rupeshkush <123472378+rupeshkush@users.noreply.github.com>
2023-03-22 09:29:04 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Rupesh Kumar!";
+        return "Hello from Spring Boot!";
     }   
 }
